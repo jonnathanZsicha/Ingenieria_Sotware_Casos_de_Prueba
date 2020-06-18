8394d7b53f841c7c96cbc342ad3a6f636e21273c
ISRAEL CHUCHUCA <israelchuchuca@MacBook-Pro-de-ISRAEL.local>
2020-06-18 14:53:57 -0500

    medico

--- src/ec/edu/ups/entidad/Medico.java
+++ src/ec/edu/ups/entidad/Medico.java
@@ -13,7 +13,12 @@ public class Medico implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	private int codigo;
+	private String nombre;
+	private String apellido;
+	private String especialidad;
+	
 	public Medico() {
 		super();
 	}
