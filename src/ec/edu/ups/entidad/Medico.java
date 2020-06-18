a272a50a8b1d5dfc498efa1aa172c9193e8564f9
ISRAEL CHUCHUCA <israelchuchuca@MacBook-Pro-de-ISRAEL.local>
2020-06-18 14:45:05 -0500

    entidad Medico

---
+++ src/ec/edu/ups/entidad/Medico.java
@@ -0,0 +1,21 @@
+package ec.edu.ups.entidad;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Medico
+ *
+ */
+@Entity
+
+public class Medico implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Medico() {
+		super();
+	}
+   
+}
